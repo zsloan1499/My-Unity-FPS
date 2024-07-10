5a8bfa7f5c1d921e81a6779c8168ca58544a28aa
Sloan7 <zachary.sloan1@marist.edu>
2024-07-09 20:30:13 -0400

    added enemies

---
+++ Assets/scripts/EnemyScripts/MeleeEnemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : BaseEnemy
+{
+
+    enum MeleeEnemyState{ IDLE, PATROL, ATTACK, DEAD, RUNNING}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
